3a766cdaa2515334f93c07486ea9222b41671fcc
Boburjon Joraboyev <139137865+boburjon1504@users.noreply.github.com>
2024-10-27 15:02:18 +0500

    Time: 15 ms (23.91%), Space: 62.2 MB (17.1%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cs
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.cs
@@ -1,5 +1,5 @@
 public class Solution {
-    public IList<IList<int>> FindDifference(int[] nums1, int[] nums2) {
-        return [nums1.Except(nums2).ToList(), nums2.Except(nums1).ToList()];
-    }
+    public IList<IList<int>> FindDifference(int[] nums1, int[] nums2) => 
+                [nums1.Except(nums2).ToList(), nums2.Except(nums1).ToList()];
+    
 }
