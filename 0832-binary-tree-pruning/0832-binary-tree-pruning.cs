347148d0ec1ff36a14200b2b394a693a800d4633
Boburjon Joraboyev <139137865+boburjon1504@users.noreply.github.com>
2024-10-30 18:31:41 +0500

    Time: 0 ms (100%), Space: 40.2 MB (56.86%) - LeetHub

---
+++ 0832-binary-tree-pruning/0832-binary-tree-pruning.cs
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode PruneTree(TreeNode root) {
+        var one = HasOne(root);
+        if(!one){
+            return null;
+        }
+        InOrder(root, null);
+
+        return root;
+    }
+
+    private void InOrder(TreeNode root, TreeNode parent){
+        if(root is null){
+            return;
+        }
+        var one = HasOne(root);
+        if(!one){
+            if(parent.left == root){
+                parent.left = null;
+            }else{
+                parent.right = null;
+            }
+        }
+        InOrder(root.left, root);
+        InOrder(root.right, root);
+    }
+
+    private bool HasOne(TreeNode root){
+        if(root is null){
+            return false;
+        }
+        if(root.val == 1){
+            return true;
+        }
+
+        return root.val == 1 || HasOne(root.left) || HasOne(root.right);
+    }
+}
