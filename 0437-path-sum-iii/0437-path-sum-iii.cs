5f4001abfd6daaf61e9452363d0151126aec12ed
Boburjon Joraboyev <139137865+boburjon1504@users.noreply.github.com>
2024-11-02 18:47:36 +0500

    Time: 57 ms (14.29%), Space: 43.1 MB (84.21%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.cs
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int PathSum(TreeNode root, int targetSum) {
+        var cnt = 0;
+
+        InOrder(root, targetSum,ref cnt);
+
+        return cnt;
+    }
+
+    public void InOrder(TreeNode root, int target,ref int cnt){
+        if(root is null){
+            return;
+        }
+        Count(root, target,0, false,ref cnt);
+
+        InOrder(root.left, target,ref cnt);
+        InOrder(root.right, target,ref cnt);
+    }
+
+    private void Count(TreeNode root, long target, long sm, bool added,ref int cnt){
+        if(root is null) return;
+
+        sm += root.val;
+        added = true;
+        if(added && sm == target){
+            cnt++;
+        }
+        Count(root.left, target, sm, added,ref cnt);
+        added = false;
+        if(root.right is null){
+            sm -= root.val;
+        }
+        Count(root.right, target, sm, added,ref cnt);
+    }
+}
