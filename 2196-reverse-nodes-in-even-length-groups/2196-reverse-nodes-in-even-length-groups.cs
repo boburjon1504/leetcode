df4a16938fe77aa81865e511bbda367bc4470a2d
Boburjon Joraboyev <139137865+boburjon1504@users.noreply.github.com>
2024-12-09 14:21:32 +0500

    Time: 35 ms (5.56%), Space: 77.7 MB (100%) - LeetHub

---
+++ 2196-reverse-nodes-in-even-length-groups/2196-reverse-nodes-in-even-length-groups.cs
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseEvenLengthGroups(ListNode head) {
+        var c = 1;
+        var temp = head;
+        var prev = temp;
+
+        while(temp is not null){
+            var t = 1;
+            var cur = temp;
+            while(cur is not null && t < c){
+                cur = cur.next;
+                t++;
+            }
+            Console.WriteLine($"{t}  {c} {temp.val}");
+            if((t % 2 == 0 && cur is not null) || (cur is null && t % 2 == 1)){
+                cur = temp;
+                ListNode pr = null;
+                ListNode tail = cur;
+                while(cur is not null && t > 0){
+                    var next = cur.next;
+                    cur.next = pr;
+                    pr = cur;
+                    cur = next;
+                    t--;
+                }
+                prev.next = pr;
+                prev = tail;
+                tail.next = cur;
+                temp = cur;
+            }else{
+                //Console.WriteLine($"{prev.val}  {(cur is not null ? cur.val : 0)} {t}");
+
+                while(temp is not null && t > 0){
+                    prev = temp;
+                    temp = temp.next;
+                    t--;
+                }
+            }
+            c++;
+        }
+
+        return head;
+    }
+}
