4fa4701d02fcf19bc30aee8e4111ac5c8351c588
Boburjon Joraboyev <139137865+boburjon1504@users.noreply.github.com>
2024-12-13 16:38:22 +0500

    Time: 13 ms (50%), Space: 49.5 MB (9.09%) - LeetHub

---
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public bool WinnerOfGame(string colors) {
+        int cA = 0, cB = 0, totalA = 0, totalB = 0;
+        
+        foreach(var i in colors){
+            if(i == 'A'){
+                cA++;
+                if(cB - 2 > 0){
+                    totalB += cB - 2;
+                }
+                cB = 0;
+            }else{
+                cB++;
+                if(cA - 2 > 0){
+                    totalA += cA - 2;
+                }
+                cA = 0;
+            }
+        }
+        if(cA - 2 > 0){
+            totalA += cA - 2;
+        }
+
+        if(cB - 2 > 0){
+            totalB += cB - 2;
+        }
+        Console.WriteLine($"{totalA}  {totalB}");
+        return totalA > totalB;
+
+    }
+}
