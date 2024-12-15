a43f2b8e1736e9a8790d68a24cc1f20ce40cd73a
Boburjon Joraboyev <139137865+boburjon1504@users.noreply.github.com>
2024-12-15 14:56:18 +0500

    Time: 4 ms (45.98%), Space: 48.1 MB (55.33%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public bool IsValidSudoku(char[][] board) {
+        var dc = new Dictionary<char, IList<IList<int>>>();
+
+        for(var i = 0; i < board.Length; i++){
+            for(var j = 0; j < board.Length; j++){
+                if(!char.IsDigit(board[i][j])){
+                    continue;
+                }
+                if(!dc.ContainsKey(board[i][j])){
+                    dc[board[i][j]] = new List<IList<int>>{
+                        new List<int>{ i, j }
+                    };
+                }else{
+                    foreach(var loc in dc[board[i][j]]){
+                        if(loc[0] == i || loc[1] == j || (loc[0] / 3 == i /3 && loc[1] / 3 == j / 3 && Math.Abs(loc[0] - i) < 3 && Math.Abs(loc[1] - j) < 3)){
+                            return false;
+                        }
+                    }
+                    dc[board[i][j]].Add([i,j]);
+                }
+
+            }
+        }
+
+        return true;
+    }
+}
