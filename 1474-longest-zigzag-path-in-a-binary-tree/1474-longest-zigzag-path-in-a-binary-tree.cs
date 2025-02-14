7e397a52e8b49bdde3dd9068aff482f700743938
Boburjon Joraboyev <139137865+boburjon1504@users.noreply.github.com>
2025-02-14 10:13:37 +0500

    Time: 6 ms (51.52%), Space: 59.2 MB (26.41%) - LeetHub

---
+++ 1474-longest-zigzag-path-in-a-binary-tree/1474-longest-zigzag-path-in-a-binary-tree.cs
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int LongestZigZag(TreeNode root) {
+        var mx = 0;
+
+        MaxZigZag(root, false, 0, ref mx);
+
+        return mx;
+    }
+
+    private void MaxZigZag(TreeNode root, bool isRight, int c,ref int mx){
+        if(root is null){
+            return;
+        }
+
+        if(isRight && root.left is null && c > mx){
+            mx = c;
+            c = 0;
+        }
+        if(!isRight && root.right is null && c > mx){
+            mx = c;
+            c = 0;
+        }
+
+        MaxZigZag(root.left, false, (isRight ? c + 1 : 1), ref mx);
+        MaxZigZag(root.right, true, (isRight ? 1 : c + 1), ref mx);
+    }
+}
