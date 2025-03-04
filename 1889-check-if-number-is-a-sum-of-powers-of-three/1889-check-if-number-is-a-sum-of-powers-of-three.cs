a7fbbd6b7f6059fcd530173a0d21046e7a348f51
Boburjon Joraboyev <139137865+boburjon1504@users.noreply.github.com>
2025-03-04 11:16:16 +0500

    Time: 346 ms (0%), Space: 47.5 MB (5.88%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cs
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.cs
@@ -8,28 +8,35 @@ public class Solution {
             power *= 3;
         }
 
-        return IsPower(n, ls, new());
+        return IsPower(n, ls, new(), new());
     }
 
     private bool IsPower(
         int n, 
         IList<int> nums, 
-        HashSet<int> comb){
-        
+        HashSet<int> comb,
+        Dictionary<int, bool> memo){
+
+        if(memo.ContainsKey(n)) return memo[n];
         if(n == 0) return true;
         if(n < 0) return false;
 
         foreach(var i in nums){
-            if(comb.Contains(i)) return false;
+            if(comb.Contains(i)){
+                memo[n] = false;
+                return false;
+            }
 
             comb.Add(i);
 
-            if(IsPower(n - i, nums, comb)){
+            if(IsPower(n - i, nums, comb, memo)){
+                memo[n] = true;
                 return true;
             }
 
             comb.Remove(i);
         }
+        memo[n] = false;
         return false;
     }
 }
