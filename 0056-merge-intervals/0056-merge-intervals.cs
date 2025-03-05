6d687a105ce323af1f7b6c087f47e03afab457ab
Boburjon Joraboyev <139137865+boburjon1504@users.noreply.github.com>
2025-03-05 12:43:22 +0500

    Time: 18 ms (10.3%), Space: 58.3 MB (5.46%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[][] Merge(int[][] intervals) {
+
+    intervals = intervals.OrderBy(x => x[0]).ThenBy(x => x[1]).ToArray();
+
+    var ans = new List<IList<int>>{ intervals[0] };
+
+    for(var i = 1; i < intervals.Length; i++){
+        var last = ans[^1];
+        var inter = intervals[i];
+
+        if(last[1] >= inter[1]){
+            continue;
+        }else if(last[1] < inter[0]){
+            ans.Add(inter);
+        }else if(last[1] >= inter[0]){
+            last[1] = inter[1];
+        }
+    }
+
+     return ans.Select(x => x.ToArray()).ToArray();   
+    }
+}
