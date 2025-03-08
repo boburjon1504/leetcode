3e03cca5789d9038baede0f082edfd957a09a5ed
Boburjon Joraboyev <139137865+boburjon1504@users.noreply.github.com>
2025-03-09 03:57:35 +0500

    Time: 968 ms (5.23%), Space: 63.1 MB (10.19%) - LeetHub

---
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int LongestOnes(int[] nums, int k) {
+        int l = 0, r = 0, mx = 0;
+        var cntZero = 0;
+        while(r < nums.Length){
+            if(nums[r] == 0){
+                Console.WriteLine($"Work: {r} {l}");
+                if(cntZero >= k){
+                    while(nums[l] != 0){
+                        l++;
+                    }
+                    l++;
+                }else{
+                    cntZero++;
+                }
+            }
+
+            var val = r - l + 1;
+
+            if(mx < val){
+                mx = val;
+            }
+            Console.WriteLine($"{r} {l} {mx}  {cntZero}");
+            r++;
+        }
+
+        return mx;
+    }
+}
