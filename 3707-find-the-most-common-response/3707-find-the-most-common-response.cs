265f0f390978b9344334727f25a406918d091a7a
Boburjon Joraboyev <139137865+boburjon1504@users.noreply.github.com>
2025-06-06 02:23:41 +0500

    Time: 467 ms (15.72%), Space: 267.3 MB (7.42%) - LeetHub

---
+++ 3707-find-the-most-common-response/3707-find-the-most-common-response.cs
@@ -0,0 +1,34 @@
+using System.Text.Json;
+
+public class Solution {
+    public string FindCommonResponse(IList<IList<string>> responses) {
+        var dc = new Dictionary<string, int>();
+        foreach(var response in responses){
+            var set = new HashSet<string>();
+            foreach(var res in response){
+                set.Add(res);
+            }
+
+            foreach(var i in set){
+                if(!dc.ContainsKey(i)){
+                    dc[i] = 0;
+                }
+                dc[i]++;
+            }
+        }
+        var mx = 0;
+        var mxString = "";
+
+        Console.WriteLine(JsonSerializer.Serialize(dc));
+        foreach(var (key, val) in dc){
+            if (val > mx){
+                mx = val;
+                mxString = key;
+            }else if (val == mx && key.CompareTo(mxString) < 0){
+                mxString = key;
+            }
+        }
+
+        return mxString;
+    }
+}
