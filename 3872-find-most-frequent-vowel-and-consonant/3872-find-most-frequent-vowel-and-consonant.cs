e9af90ac9291ef09cf2179c771fe9ffe08cbc839
Boburjon Joraboyev <139137865+boburjon1504@users.noreply.github.com>
2025-06-11 20:48:03 +0500

    Time: 2 ms (65.23%), Space: 40.9 MB (80.47%) - LeetHub

---
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int MaxFreqSum(string s) {
+        var freq = new Dictionary<char, int>();
+
+        int maxV = 0, maxC = 0;
+
+        foreach(var i in s){
+            if(!freq.ContainsKey(i)){
+                freq[i] = 0;
+            }
+
+            freq[i]++;
+
+            if("aeiou".Contains(i)){
+                maxV = Math.Max(maxV, freq[i]);
+            }else{
+                maxC = Math.Max(maxC, freq[i]);
+            }
+        }
+
+        return maxV + maxC;
+    }
+}
